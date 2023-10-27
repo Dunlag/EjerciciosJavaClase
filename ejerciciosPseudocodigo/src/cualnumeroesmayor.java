86565624140c7356f0a94dd0f6425a5a1200a870
Dunlag <fernando.pinilla85@gmail.com>
2023-10-27 17:57:07 +0200

    subido por la web

---
+++ ejerciciosPseudocodigo/src/cualnumeroesmayor.java
@@ -0,0 +1,21 @@
+public class cualnumeroesmayor {
+    public static void main(String[] args) {
+        int a,b,c;
+        a = 15;
+        b= 2;
+        c = 100000;
+        if (a>=b){
+            if (a>=c){
+                System.out.println("el numero a "+a+" es el mayor");
+
+            } else {
+                System.out.println("el numero c " + c + " es el mayor");
+            }
+        } else if (b>=c) {
+            System.out.println("el numero b " + b +" es el mayor");
+        }
+        else {
+            System.out.println("el numero c"+ c+ " es el mayor");
+        }
+    }
+}
---
+++ ejerciciosPseudocodigo/src/pedirnumeroporteclado.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class pedirnumeroporteclado {
+    public static void main(String[] args) {
+        int num=0;
+        Scanner scanner= new Scanner(System.in);
+        do {
+            System.out.println("dime un numero positivo");
+            num = scanner.nextInt();
+        }while (num<=0);
+    }
+}
