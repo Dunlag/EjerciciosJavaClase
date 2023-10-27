bacdb72ee0c823676260bae6e3ad3c9a012fc5fa
Dunlag <fernando.pinilla85@gmail.com>
2023-10-27 17:47:58 +0200

    nose

--- Caso Practico 02/src/numerosdel10anegativo.java
+++
@@ -1,8 +0,0 @@
-public class numerosdel10anegativo {
-    public static void main(String[] args) {
-        int cont;
-        for (cont=10;cont>=-1;cont--){
-            System.out.print(cont+ ", ");
-        }
-    }
-}
